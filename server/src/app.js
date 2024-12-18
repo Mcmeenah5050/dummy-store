ba6b0340bd5e3ef71f4b1e80c942e24617ecaf8a
mcmeenah5050 <abidemiakande1@gmail.com>
2024-12-18 11:27:24 +0100

    added client url cors

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,11 @@ import orderRoutes from "./routes/order.js";
 
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:5174", "http://localhost:5175"],
+  origin: [
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "https://dummy-store-gamma.vercel.app/",
+  ],
   optionsSuccessStatus: 200,
   credentials: true,
 };
